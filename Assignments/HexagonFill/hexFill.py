872dee0932cb105a6a0caf4cb3500772f5ea3b30
Ali Akbar <aliakbar09a@gmail.com>
2018-10-11 01:56:27 +0530

    done but not working for angled hexagon

--- Assignments/HexagonFill/hexFill.py
+++ Assignments/HexagonFill/hexFill.py
@@ -9,17 +9,16 @@ height = int(data[1].split()[-1])
 xC = int(data[2].split()[-1])
 yC = int(data[3].split()[-1])
 a = int(data[4].split()[-1])
-fi = float(data[5].split()[-1])
 o.close()
-
-fi = fi*(22.0/7.0)/180.0
 
 def draw_x(mat):
     for i in range(width):
-        mat[int(height/2)][i] = '50 '
+        if mat[int(height/2)][i] == '10 ':
+            mat[int(height/2)][i] = '50 '
 def draw_y(mat):
     for i in range(height):
-        mat[i][int(width/2)] = '50 '
+        if mat[i][int(width/2)] == '10 ':
+            mat[i][int(width/2)] = '50 '
 # modulus function
 def mod(a):
     if(a < 0):
@@ -30,6 +29,7 @@ def rotate(x, y, theta):
     xh = int(round(x*m.cos(theta) - y*m.sin(theta)))
     yh = int(round(x*m.sin(theta) + y*m.cos(theta)))
     return xh, yh
+
 # function to shift the hexagon points
 def shift(x, y):
     x = int(width/2 + x)
@@ -50,17 +50,7 @@ def drawLine(mat, x1, y1, x2, y2):
         y1 = y1 + yinc
         mat[int(round(y1))][int(round(x1))] = '255 '
 
-def main():
-    mat = [['10 ' for i in range(width)] for j in range(height)]
-    x, y = rotate(a, 0, fi)
-    for i in range(6):
-        theta = 22.0/21.0
-        xh, yh = rotate(x, y, theta)
-        # print(x, y, xh, yh, theta)
-        xShift, yShift = shift(x + xC, y + yC)
-        xhShift, yhShift = shift(xh + xC, yh +yC)
-        drawLine(mat, xShift, yShift, xhShift, yhShift)
-        x, y = xh, yh
+def writeToFile(mat):
     # opening the pgm file
     filename = 'hex.pgm'
     f = open(filename, 'w')
@@ -72,5 +62,75 @@ def main():
     for i in range(height):
         f.writelines(mat[i])
         f.write('\n')
+def nchk(x):
+    if x == '255 ':
+        return False
+    else:
+        return True
+def checkThreePixel(mat, j, i):
+    found = 0
+    if chk(mat[j][i-1]):
+        found = found + 1
+    if chk(mat[j][i]):
+        found = found + 1
+    if chk(mat[j][i+1]):
+        found = found + 1
+    return found
+def chk(x):
+    if x == '255 ':
+        return True
+    else:
+        return False
+def polyFill(mat):
+    for j in range(height):
+        points = []
+        # boundary condition
+        if chk(mat[j][0]) and nchk(mat[j][1]):
+            points.append(0)
+        for i in range(1, width-1):
+            upper_found, lower_found = 0, 0
+            if len(points)%2 == 0:
+                if chk(mat[j][i]) and nchk(mat[j][i+1]):
+                    # intersection point found
+                    # now checking if it has to be counted once or twice
+                    upper_found = checkThreePixel(mat, j-1, i)
+                    lower_found = checkThreePixel(mat, j+1, i)
+                    points.append(i)
+                    if upper_found > 1 and lower_found == 0:
+                        points.append(i)
+                    if lower_found > 1 and upper_found == 0:
+                        points.append(i)
+            else:
+                if chk(mat[j][i]) and nchk(mat[j][i-1]):
+                    # intersection point found
+                    # now checking if it has to be counted once or twice
+                    upper_found = checkThreePixel(mat, j-1, i)
+                    lower_found = checkThreePixel(mat, j+1, i)
+                    points.append(i)
+                    if upper_found > 1 and lower_found == 0:
+                        points.append(i)
+                    if lower_found > 1 and upper_found == 0:
+                        points.append(i)
+        if chk(mat[j][width-1]) and nchk(mat[j][width-2]):
+            points.append(i)
+        print(points)
+        pair = 0
+        while pair < len(points):
+            for point in range(points[pair] + 1, points[pair+1]):
+                mat[j][point] = '100 '
+            pair = pair + 2
+
+def main():
+    mat = [['10 ' for i in range(width)] for j in range(height)]
+    x, y = rotate(a, 0, 0)
+    for i in range(6):
+        theta = 22.0/21.0
+        xh, yh = rotate(x, y, theta)
+        xShift, yShift = shift(x + xC, y + yC)
+        xhShift, yhShift     = shift(xh + xC, yh +yC)
+        drawLine(mat, xShift, yShift, xhShift, yhShift)
+        x, y = xh, yh
+    polyFill(mat)
+    writeToFile(mat)
 # executing main()
 main()
